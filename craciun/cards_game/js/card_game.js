5ef6e1133491b468e64cb8e6e6f84b8ec06d7679
WavyDragonss <117162912+WavyDragonss@users.noreply.github.com>
2025-12-12 12:39:48 +0200

    Update card_game.js

--- craciun/cards_game/js/card_game.js
+++ craciun/cards_game/js/card_game.js
@@ -155,7 +155,7 @@
 
   // set your password and finish page here:
   const DEBUG_PASSWORD = '0000';
-  const FINISH_PAGE_URL = '../final_message/finish.html';
+  const FINISH_PAGE_URL = '../wheel_game/wheel.html';
 
   if (debugBtn) {
     debugBtn.addEventListener('click', () => {
